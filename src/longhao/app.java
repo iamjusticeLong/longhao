b29c33770e5052f57ff4455a2d251fba39f7d890
kesa <1142638423@qq.com>
2018-11-29 11:44:35 +0800

    second commit longhao

--- src/longhao/app.java
+++ src/longhao/app.java
@@ -2,6 +2,14 @@ package longhao;
 
 public class app {
 	private int id;
+	private String name;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public int getId() {
 		return id;
